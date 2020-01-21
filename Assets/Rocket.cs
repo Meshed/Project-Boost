455eed0eb8e31e474587ad117fde9448b2005da0
Mark Brown <marksbrown@gmail.com>
2020-01-21 14:57:19 -0500

    Add sound for rocket thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody _rigidBody;
+    AudioSource _audioSource;
+
+    enum RocketRotation
+    {
+        Left,
+        Right
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,15 +32,42 @@ public class Rocket : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             _rigidBody.AddRelativeForce(Vector3.up);
+
+            PlayRocketThrustSound();
+        }
+        else
+        {
+            _audioSource.Stop();
         }
 
         if(Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            RotateRocket(RocketRotation.Left);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            RotateRocket(RocketRotation.Right);
+        }
+    }
+
+    private void RotateRocket(RocketRotation rocketRotation)
+    {
+        switch(rocketRotation)
+        {
+            case RocketRotation.Left:
+                transform.Rotate(Vector3.forward);
+                break;
+            case RocketRotation.Right:
+                transform.Rotate(-Vector3.forward);
+                break;
+        }
+    }
+
+    private void PlayRocketThrustSound()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
         }
     }
 }
